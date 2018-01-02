4e490b4333df2f05e41416eb5d1ed9ec7ee1b1b4
Thisseas X <thisseasx@yahoo.com>
2018-01-02 06:12:23 +0200

    Feature 1 added

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -6,5 +6,6 @@ public class TestClass {
         System.out.println("Hello World!");
         System.out.println("Hello World!2");
         System.out.println("Hello World!3");
+        System.out.println("Hello World!FEATURE1");
     }
 }
